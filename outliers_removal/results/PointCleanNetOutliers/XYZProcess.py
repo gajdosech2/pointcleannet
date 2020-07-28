378bdbae85e3b60887900aa2ca312273a293d25d
gajdosech2 <l.gajdosech@gmail.com>
2020-07-28 15:05:35 +0200

    gpu dataset

--- outliers_removal/results/PointCleanNetOutliers/XYZProcess.py
+++ outliers_removal/results/PointCleanNetOutliers/XYZProcess.py
@@ -1,9 +1,9 @@
-read = open("outliers_value_galera100k_outliers_gaussian_0.info")
+read = open("outliers_value_gpu_000.info")
 write = open("pc.xyz", 'w')
 
 for line in read:
     splited = line.split(' ')
-    if (float(splited[3]) < 0.5):
+    if (float(splited[3]) == 0):
     	print(splited[0] + ' ' + splited[1] + ' ' + splited[2], file=write)
     
 read.close()
